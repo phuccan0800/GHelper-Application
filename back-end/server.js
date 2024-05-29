b645b72e95a04ea318762bc3384e4658c4fc9f15
Truong Tan Phuc <phuccan0800@gmail.com>
2024-05-30 02:20:52 +0700

    add msql
    - install mysql2

--- back-end/server.js
+++ back-end/server.js
@@ -1,12 +1,18 @@
 require('dotenv').config();
+const mysql = require('mysql2');
 const express = require('express');
 const app = express();
 const host = process.env.HOST || 3000;
 const port = process.env.PORT || "localhost";
 const configViewEngine = require('./config/viewEngine');
 const webRoute = require('./src/routes/web');
+const connection = mysql.createConnection({
+    host: "115.146.126.73",
+    user: "GHelper",
+    password: "5GdpfZsmHbXeGXJH",
+    database: "ghelper"
+})
 configViewEngine(app);
-
 app.use('/', webRoute);
 
 
