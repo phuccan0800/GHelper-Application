807fe1817a51370d092496e67e92ab9044a07a42
Truong Tan Phuc <phuccan0800@gmail.com>
2024-06-22 21:19:01 +0700

    - Add Email Service
    Using Gmail SMTP for send email to user.

---
+++ back-end/src/services/email.service.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer');
+
+// Create a transporter using Gmail SMTP
+const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'phucttgcd210070@fpt.edu.vn',
+        pass: 'aufs nwph gpzs dodz',
+    },
+});
+
+// Function to send reset password email
+const resetPassword = async (email) => {
+    try {
+        const code = Math.floor(100000 + Math.random() * 900000);
+        const mailOptions = {
+            from: 'phucttgcd210070@fpt.edu.vn',
+            to: email,
+            subject: `Reset Password: ${code}`,
+            html: `<p>Your reset code is: <b>${code}</b>.</p><p>Best regards,<br>GHelper</p>`,
+        };
+
+        // Send the email
+        await transporter.sendMail(mailOptions);
+        console.log(`Reset password email sent successfully to email ${email}`);
+    } catch (error) {
+        console.error('Error sending reset password email:', error);
+    }
+};
+
+module.exports = {
+    resetPassword,
+};
