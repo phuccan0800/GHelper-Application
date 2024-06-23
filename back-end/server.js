15fddf1e761da1e77d72073955e8c7bf0a319e81
Truong Tan Phuc <phuccan0800@gmail.com>
2024-06-23 16:41:05 +0700

    Update server.js
    using Redis

--- back-end/server.js
+++ back-end/server.js
@@ -1,17 +1,23 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
-const host = process.env.HOST || "localhost";
-const port = process.env.PORT || 3000;
+
 const configViewEngine = require('./src/config/viewEngine');
 const apiRoute = require('./src/routes/api');
 const connection = require('./src/config/database');
+const redis = require('./src/config/redis');
 const rateLimiter = require('./src/middlewares/rateLimit.middleware');
+const blockUnknownDevices = require('./src/middlewares/blockUnknownDevices.middleware');
 
+const app = express();
+const host = process.env.HOST || "localhost";
+const port = process.env.PORT || 3000;
 
 connection();
+redis.connectRedis();
+
 configViewEngine(app);
+app.use(blockUnknownDevices);
 app.use(rateLimiter);
 app.use(express.json());
 app.use('/api', apiRoute);
