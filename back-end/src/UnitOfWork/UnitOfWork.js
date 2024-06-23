de140cb57fbb4d40cb2a8021dff8d3bf8851b2f2
Truong Tan Phuc <phuccan0800@gmail.com>
2024-06-23 16:38:50 +0700

    Add redis into Project
    - Add redis URL into .env
    - Add some function using redis into UnitofWork

--- back-end/src/UnitOfWork/UnitOfWork.js
+++ back-end/src/UnitOfWork/UnitOfWork.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
+
+const redis = require('../config/redis');
 const userRepository = require('../repositories/users.repository');
 const userRoleRepository = require('../repositories/userRole.repository')
 
 class UnitOfWork {
     constructor() {
         this.session = null;
+        this.redis = redis;
         this.repositories = {
             userRepository: userRepository,
             userRoleRepository: userRoleRepository
@@ -28,6 +31,27 @@ class UnitOfWork {
     async rollback() {
         await this.session.abortTransaction();
         this.session.endSession();
+    }
+
+    async getUserSessionByToken(token) {
+        const session = await this.redis.client.get(token);
+        return JSON.parse(session);
+    }
+
+    async saveUSerSession(userId, token) {
+        await this.redis.client.rPush(userId.toString(), token);
+    }
+
+    async getSessionTokens(userId) {
+        return await this.redis.client.lRange(userId.toString(), 0, -1);
+    }
+
+    async removeSessionToken(userId, token) {
+        await this.redis.client.lRem(userId.toString(), 0, token);
+    }
+
+    async removeDeviceToken(token) {
+        await this.redis.client.del(token);
     }
 }
 
