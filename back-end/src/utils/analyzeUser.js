6969e3ca1525a0f7e06169990b7e271e9a6c20ec
Truong Tan Phuc <phuccan0800@gmail.com>
2024-06-23 16:39:48 +0700

    utils: Add analyze user
    - Add utils for analyze user into back-end

---
+++ back-end/src/utils/analyzeUser.js
@@ -0,0 +1,35 @@
+const useragent = require('express-useragent');
+const requestIp = require('request-ip');
+const geoip = require('geoip-lite');
+
+const analyzeUser = (req) => {
+    const userAgent = req.headers["user-agent"] || null;
+    const ip = requestIp.getClientIp(req) || null;
+    const geo = geoip.lookup("116.98.119.10") || {};
+    const { city, region, country, ll: coordinates } = geo;
+    if (!userAgent) {
+        return null;
+    }
+    userAgentParser = useragent.parse(userAgent);
+
+    if (userAgentParser.os === 'unknown' || userAgentParser.platform === 'unknown') {
+        return null;
+    }
+    const deviceInfo = {
+        userAgent,
+        ip,
+        location: {
+            city,
+            region,
+            country,
+            coordinates
+        },
+        os: userAgentParser.os,
+        browser: userAgentParser.browser,
+        version: userAgentParser.version,
+        platform: userAgentParser.platform,
+    };
+    return deviceInfo;
+};
+
+module.exports = analyzeUser;
