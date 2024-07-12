5c7da1e12f8fc5bc0582f17d455b52ddcc03ff54
Truong Tan Phuc <phuccan0800@gmail.com>
2024-07-12 20:07:15 +0700

    Add refresh for home screen

--- Uber-clone/screens/Home.js
+++ Uber-clone/screens/Home.js
@@ -1,5 +1,6 @@
+import React, { useCallback, useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Image, TextInput, RefreshControl, ScrollView } from 'react-native';
 import MapboxPlacesAutocomplete from "react-native-mapbox-places-autocomplete";
 import tw from 'twrnc';
 
@@ -9,51 +10,85 @@ import { setDestination, setOrigin } from '../slices/navSlice';
 import NavOptions from '../components/NavOptions';
 import TranslateButton from '../components/TranslateButton';
 
+import styles from '../assets/styles';
+
 const Home = () => {
+  const [refreshing, setFreshing] = useState(false);
+  const [data, setData] = useState([]);
   const dispatch = useDispatch();
+
+  const handleRefresh = async () => {
+  };
+
+  const onRefresh = useCallback(async () => {
+    setFreshing(true);
+    await handleRefresh();
+    setFreshing(false);
+  }, []);
+
   return (
-    < SafeAreaView style={tw`bg-white h-full`}>
-      < View style={tw`p-5`}>
-        <Text style={{ marginBottom: 20, marginTop: 50, fontStyle: "bold", fontSize: 25, }}>G-HELPER</Text>
-        {/* <Image
+    < SafeAreaView >
+      <ScrollView
+        // contentContainerStyle={localStyles.scrollView}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+          />
+        }>
+
+        < View style={tw`p-5`}>
+          <Text style={{ marginBottom: 20, marginTop: 50, fontStyle: "bold", fontSize: 25, }}>G-HELPER</Text>
+          {/* <Image
           style={{ width: 100, height: 100, resizeMode: "contain" }}
           source={{
             uri: "https://links.papareact.com/gzs"
           }} /> */}
-        <MapboxPlacesAutocomplete
-          id="origin"
-          placeholder="Where you want to go ?"
-          accessToken='sk.eyJ1IjoicGh1Y2NhbjA4MDAiLCJhIjoiY2x4Ym8waGsyMGhhbjJtczN4Nmlub2dsNiJ9.WYadawgbd5uxuSli04nckQ'
-          onPlaceSelect={(data) => {
-            dispatch(setOrigin({
-              location: data.geometry.coordinates,
-              destination: null
-            }));
-            console.log(data.geometry.coordinates)
-            dispatch(setDestination(null));
-          }}
-          onClearInput={({ id }) => {
-            id === "origin" && dispatch(setOrigin({
-              location: null
-            }));
-          }}
-          countryId="VN"
-          containerStyle={{
-            marginBottom: 0,
-            containerStyle: {
-              flex: 0,
-            },
-            textInput: {
-              fontSize: 18,
-            }
-          }} />
-        <TranslateButton />
-      </View >
+          <MapboxPlacesAutocomplete
+            id="origin"
+            placeholder="Where you want to go ?"
+            accessToken='sk.eyJ1IjoicGh1Y2NhbjA4MDAiLCJhIjoiY2x4Ym8waGsyMGhhbjJtczN4Nmlub2dsNiJ9.WYadawgbd5uxuSli04nckQ'
+            onPlaceSelect={(data) => {
+              dispatch(setOrigin({
+                location: data.geometry.coordinates,
+                destination: null
+              }));
+              console.log(data.geometry.coordinates)
+              dispatch(setDestination(null));
+            }}
+            onClearInput={({ id }) => {
+              id === "origin" && dispatch(setOrigin({
+                location: null
+              }));
+            }}
+            countryId="VN"
+            containerStyle={{
+              marginBottom: 0,
+              containerStyle: {
+                flex: 0,
+              },
+              textInput: {
+                fontSize: 18,
+              }
+            }} />
+          <TranslateButton />
+        </View >
 
-      <NavOptions />
+        <NavOptions />
+      </ScrollView>
     </SafeAreaView >
 
   );
 };
-
+const localStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollView: {
+    flex: 1,
+    backgroundColor: 'pink',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 export default Home;
