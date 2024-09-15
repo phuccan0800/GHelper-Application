8c96f662ad1c82b4a5fc2cc86e83c7b19b0b9365
Truong Tan Phuc <phuccan0800@gmail.com>
2024-09-15 19:46:37 +0700

    ...

--- Worker/screens/HomeScreen/HomeScreen.js
+++ Worker/screens/HomeScreen/HomeScreen.js
@@ -1,7 +1,8 @@
 import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
 import MapView from 'react-native-maps'
 import MapViewDirections from 'react-native-maps-directions';
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import * as Location from 'expo-location';
 
 const HomeScreen = () => {
     const [state, setState] = useState({
@@ -16,17 +17,39 @@ const HomeScreen = () => {
     })
 
     const { pickupCords, droplocationCors } = state
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            console.log(location)
+            setLocation(location);
+        })();
+    }, []);
+
+    let text = 'Waiting..';
+    if (errorMsg) {
+        text = errorMsg;
+    } else if (location) {
+        text = JSON.stringify(location);
+    }
     return (
         <SafeAreaView style={styles.container}>
             <Text>HomeScreen</Text>
             <MapView
                 style={StyleSheet.absoluteFill}
-                initialRegion={{
-                    latitude: 16.057827507259542,
-                    longitude: 108.22105232324897,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
-                }}
+                showsMyLocationButton={true}
+                showsUserLocation
+                showsTraffic
+                initialRegion={location}
             >
                 {/* <MapViewDirections
                     origin={state.pickupCords}
