eee74bd464d88eb38423d754e81029b44cbcd795
Zinh <phuccan0800@gmail.com>
2024-11-01 02:38:07 +0700

    fix bug backend
    when user request with wrong email format, it's will return error

--- back-end/src/controllers/auth.controller.js
+++ back-end/src/controllers/auth.controller.js
@@ -48,7 +48,10 @@ const login = async (req, res) => {
 const forgotPassword = async (req, res) => {
     try {
         const { email } = req.body;
-
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ message: 'Invalid email format' });
+        }
         const user = await User.findOne({
         });
         if (user) {
