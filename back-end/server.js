7d283423ad61b12366a18afd11b59be548709f19
Zinh <phuccan0800@gmail.com>
2024-12-11 03:47:16 +0700

    add cron task

--- back-end/server.js
+++ back-end/server.js
@@ -11,12 +11,16 @@ const redisClient = require('./src/config/redis').client;
 const rateLimiter = require('./src/middlewares/rateLimit.middleware');
 const blockUnknownDevices = require('./src/middlewares/blockUnknownDevices.middleware');
 const { setupWebSocket } = require('./src/config/websocket');
+const startReviewAggregationCron = require('./src/cron/aggregateReview.cron');
+const startBalanceAllocationCron = require('./src/cron/allocateBalance.cron');
+
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 const host = process.env.HOST || "localhost";
 const port = process.env.PORT || 3000;
+
 
 connection();
 redis.connectRedis();
@@ -29,26 +33,8 @@ app.use('/api', apiRoute);
 app.use(bodyParser.json());
 
 setupWebSocket(wss);
-
-// const checkWorkerStatus = async () => {
-//     try {
-//         const keys = await redisClient.keys('worker:*');
-//         const now = Date.now();
-
-//         for (const key of keys) {
-//             const workerData = JSON.parse(await redisClient.get(key));
-//             if (workerData && now - workerData.lastUpdated > 60000) { // 60 giây không gửi heartbeat
-//                 await redisClient.del(key);
-//                 console.log(`Worker automatically marked offline: ${key}`);
-//             }
-//         }
-//     } catch (error) {
-//         console.error('Error in worker status check:', error);
-//     }
-// };
-
-// // Thiết lập cron job chạy mỗi 30 giây
-// setInterval(checkWorkerStatus, 30000);
+startReviewAggregationCron();
+startBalanceAllocationCron();
 
 server.listen(port, host, () => {
     console.log(`Example app listening on http://${host}:${port}`);
